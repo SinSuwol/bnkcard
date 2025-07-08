3b1b44b98225f64db61885fbf038e8e19e0e81f7
fhqhzkq99 <fhqhzkq99@gmail.com>
2025-07-08 14:23:57 +0900

    db명 변경

--- src/main/java/com/busanbank/card/admin/controller/AdminCardController.java
+++ src/main/java/com/busanbank/card/admin/controller/AdminCardController.java
@@ -1,8 +1,11 @@
 package com.busanbank.card.admin.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/admin/card")
 @RestController	
 public class AdminCardController {
-
+	
+	
 }
