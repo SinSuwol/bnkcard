faaa9a756727aa4472bd2904c300f398e02d05fe
fhqhzkq99 <fhqhzkq99@gmail.com>
2025-07-08 12:20:18 +0900

    firstcommit

--- src/main/java/com/busanbank/card/admin/controller/AdminController.java
+++ src/main/java/com/busanbank/card/admin/controller/AdminController.java
@@ -1,9 +1,13 @@
 package com.busanbank.card.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
 
-	
+	@GetMapping("/adminCardList")
+	public String adminCardList() {
+		return "admin/adminCardList";
+	}
 }
