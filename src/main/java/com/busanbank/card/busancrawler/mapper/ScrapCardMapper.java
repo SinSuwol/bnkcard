025c082e14740e5ec15a0f444c5e844472617165
fhqhzkq99 <fhqhzkq99@gmail.com>
2025-07-17 11:23:47 +0900

    관리자 스크래핑페이지 기능완

--- src/main/java/com/busanbank/card/busancrawler/mapper/ScrapCardMapper.java
+++ src/main/java/com/busanbank/card/busancrawler/mapper/ScrapCardMapper.java
@@ -1,10 +1,16 @@
 package com.busanbank.card.busancrawler.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.busanbank.card.busancrawler.dto.ScrapCardDto;
 
 @Mapper
 public interface ScrapCardMapper {
-	void insertCard(ScrapCardDto card);
+	public void insertCard(ScrapCardDto card); //리스트 삽입
+	
+	public List<ScrapCardDto> getScrapList(); //리스트 조회
+	
+	int deleteAllCards(); // 전체 삭제
 }
