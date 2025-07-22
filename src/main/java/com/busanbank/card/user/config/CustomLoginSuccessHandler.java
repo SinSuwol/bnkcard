391a71bc9f4d6d8bb4aa6093c16dc9c563fe0ae7
smallomg <ldy4081@naver.com>
2025-07-22 12:03:54 +0900

    123
    123

--- src/main/java/com/busanbank/card/user/config/CustomLoginSuccessHandler.java
+++ src/main/java/com/busanbank/card/user/config/CustomLoginSuccessHandler.java
@@ -28,7 +28,7 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler  
 		//2025-07-15 suwol
 		session.setAttribute("loginMemberNo", userDetails.getMemberNo());
 		//왜 이게 없는거지?
-		//session.setAttribute("loginUser", userDetails); 
+		session.setAttribute("loginUser", userDetails); 
 		
 		response.sendRedirect("/user/mypage");
 	}
